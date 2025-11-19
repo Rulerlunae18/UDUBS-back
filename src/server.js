3797e60be2bf35ae65e50953d6d9cee98a0265ec
Rulerlunae18 <sivenukolga@gmail.com>
2025-11-19 22:02:46 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -71,7 +71,7 @@ app.use(
     httpOnly: true,
     secure: true,
     sameSite: 'none',
-    domain: '.onrender.com',
+    domain: '.udubs-back.onrender.com',
     maxAge: 1000 * 60 * 60 * 6,
   })
 );
